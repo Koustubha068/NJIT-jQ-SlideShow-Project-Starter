0f949c7d8f5929a20b4b5588b913857e1b46e55a
Koustubha068 <KoustuK@gmail.com>
2024-11-20 21:27:11 -0500

    finished Next Photo and prev photo

--- gallery.js
+++ gallery.js
@@ -63,15 +63,29 @@ function swapPhoto() {
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
-function showNextPhoto () {
+function showNextPhoto() {
   // Increment mCurrentIndex and call swapPhoto()
+  mCurrentIndex++
+
+
   // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
+
+
+  if (mCurrentIndex === mImages.length) {
+    mCurrentIndex = 0;
+  }
+  swapPhoto()
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
-function showPrevPhoto () {
+function showPrevPhoto() {
   // Decrement mCurrentIndex and call swapPhoto()
+  mCurrentIndex--;
   // Ensure it loops to the end if mCurrentIndex is less than 0
+  if (mCurrentIndex < 0) {
+    mCurrentIndex = 9 // Last image
+  }
+  swapPhoto()
 }
 
 // Starter code for the timer function
